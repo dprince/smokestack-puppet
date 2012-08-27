3b921e63ae5b5b34d6a3bbf0eeed16ccb242de26
Dan Prince <dprince@redhat.com>
2012-08-27 11:16:08 -0400

    Add module for base SmokeStack worker.

---
+++ modules/smokestack/manifests/worker.pp
@@ -0,0 +1,63 @@
+#
+# Base SmokeStack worker configuration
+#
+#
+class smokestack::worker (
+  $username='smokestack',
+  $home_dir='/home/smokestack',
+  $installation_dir='/var/lib/libvirt/images/'
+) {
+
+  # base packages required by all SmokeStack workers
+  package { ['rubygems',
+             'ruby-devel',
+              'gcc',
+              'gcc-c++',
+              'git',
+              'rubygem-bundler',
+              'monit',
+              'mysql-devel',
+              'ntp']:
+    ensure => 'present'
+  }
+
+  service {'ntpd':
+    ensure  => 'running',
+    enable  => true,
+    require => Package["ntp"]
+  }
+
+  user { $username:
+    ensure => present
+  }
+
+  file { $home_dir:
+    ensure  => directory,
+    mode    => '700',
+    owner   => 'smokestack',
+    group   => 'smokestack',
+    require => User['smokestack']
+  }
+
+  # Capistrano directory tree
+  file { ['/u/',
+          '/u/apps/',
+          '/u/apps/SmokeStack/',
+          '/u/apps/SmokeStack/releases',
+          '/u/apps/SmokeStack/shared']:
+    ensure  => directory,
+    mode    => '775',
+    owner   => 'smokestack',
+    group   => 'smokestack',
+    require => User['smokestack']
+  }
+
+  # Disable tty requirement for sudo
+  augeas{"set_requiretty":
+    context   => "/files/etc/sudoers/",
+    changes   => [
+        "clear Defaults/requiretty/negate"
+      ]
+  }
+
+}
