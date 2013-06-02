daaeab9c75c66ae2b62cc157f323026c138e354b
Dan Prince <dprince@redhat.com>
2013-06-02 12:43:02 -0400

    Bootstrap updates.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,9 @@ IP=$1
 
 PUB_KEY=$(cat ~/.ssh/id_rsa.pub)
 
-ssh root@$IP bash <<-"EOF_BASH"
-[ ! -f ~/.ssh/id_rsa ] && ssh-keygen -N '' -t rsa -q
+ssh root@$IP bash <<-EOF_BASH
+[ ! -d ~/.ssh ] && ssh-keygen -N '' -t rsa -q
 if [ ! -f ~/.ssh/authorized_keys ]; then
  echo $PUB_KEY > .ssh/authorized_keys
 fi
-yum update -y
-yum install -y puppet
-yum install -y git
-git clone git://github.com/dprince/smokestack-puppet.git
-puppet apply --modulepath=`pwd`/smokestack-puppet/modules -e 'node default {class { "smokestack::worker": }}'
 EOF_BASH
