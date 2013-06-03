9daa818c60a75ae3503e06efdffaff25d2f56d8f
Dan Prince <dprince@redhat.com>
2013-06-03 12:56:38 -0400

    Add mirror config.

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,3 +8,37 @@ node default {
   class { 'packagekit::cron': }
 
 }
+
+node /^mirror.*/ inherits default {
+
+  class { 'smokestack::yum::mirror': }
+
+  smokestack::yum::repo { 'f18-x86_64-updates':
+   description => "Fedora 18 - x86_64 - Updates",
+   mirrorlist => 'https://mirrors.fedoraproject.org/metalink?repo=updates-released-f18&arch=x86_64',
+   cron_hour => 1,
+   cron_minute => 15,
+  }
+
+  smokestack::yum::repo { 'f18-x86_64-release':
+   description => "Fedora 18 - x86_64 - Release",
+   mirrorlist => 'https://mirrors.fedoraproject.org/metalink?repo=fedora-18&arch=x86_64',
+   cron_hour => 2,
+   cron_minute => 15,
+  }
+
+  smokestack::yum::repo { 'centos-6.4-x86_64-updates':
+   description => "CentOS-6.4 - Updates",
+   mirrorlist => 'http://mirrorlist.centos.org/?release=6.4&arch=x86_64&repo=updates',
+   cron_hour => 3,
+   cron_minute => 15,
+  }
+
+  smokestack::yum::repo { 'centos-6.4-x86_64-base':
+   description => "CentOS-6.4 - Base",
+   mirrorlist => 'http://mirrorlist.centos.org/?release=6.4&arch=x86_64&repo=os',
+   cron_hour => 4,
+   cron_minute => 15,
+  }
+
+}
